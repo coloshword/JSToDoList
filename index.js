ce635bfc1adf7df7cdd84df4b20365793204a461
coloshword <aceliang2001@gmail.com>
2021-01-19 11:44:36 -0500

    Some basic js

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello world");
+var b = "smoothie";
+console.log(b);
+document.getElementById("text").innerHTML = "Yo wassup";
+var age = prompt("What is your age?");
+document.getElementById("age").innerHTML = "Your age is " + age; 
