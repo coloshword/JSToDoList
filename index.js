0c5a735b2eb07d558bd2fa6050feaf6aa98012ef
coloshword <aceliang2001@gmail.com>
2021-01-25 14:45:22 -0500

    added event listener with enter so you can press enter to click button

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// function that calls a button click when enter is pressed
+function checkEnter() {
+    var input = document.getElementById("enter");
+    input.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+       event.preventDefault();
+       document.getElementById("btn").click();
+      }
+    });
+}
+
+checkEnter()
+
 function getInput() {
     var str = document.getElementById("enter").value;
+    document.getElementById("enter").value = "";
     var h6 = document.createElement("h6");
     h6.append(str);
     var li = document.createElement("li");
