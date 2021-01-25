cc6964b9678facc0a7c076c47c931455d8baae8f
coloshword <aceliang2001@gmail.com>
2021-01-25 14:25:16 -0500

    inputs now displayed as list

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function getInput() {
     var str = document.getElementById("enter").value;
     var h6 = document.createElement("h6");
-    h6.setAttribute("id", "item1");
     h6.append(str);
-    document.getElementById("list").appendChild(h6);
+    var li = document.createElement("li");
+    li.appendChild(h6);
+    document.getElementById("list").appendChild(li);
 }
